d0502a69a1770cf38ebe932905acbc85a7869376
edge <popikov.ev@yandex.ru>
2025-04-18 08:38:26 +0300

    fix:t

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div style={{ whiteSpace: "pre" }}>
-      initData: {window.Telegram.WebApp.initDataUnsafe.id || "not found"}
+      initData: {window.Telegram.WebApp.initData || "not found"}
     </div>
   );
 }
